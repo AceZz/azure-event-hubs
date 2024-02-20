923f5b0fab719760241e9af4b860180d3600ee15
Tristan <tristan.stampfler@ms.d-one.ai>
2024-02-20 15:36:32 +0100

    docs(root): add descriptions to variables

--- variables.tf
+++ variables.tf
@@ -1,15 +1,19 @@
 variable "environment" {
-  type = string
+  type        = string
+  description = "name of the environment (e.g. demo, dev, test, prod)"
 }
 
 variable "owner" {
-  type = string
+  type        = string
+  description = "owner of the resource group and databricks workspace"
 }
 
 variable "region" {
-  type = string
+  type        = string
+  description = "Azure region for the services"
 }
 
 variable "subscription_id" {
-  type = string
+  type        = string
+  description = "Azure subscription id to use for the services"
 }
